e3f54eebaba8cf2d482259fbac93ea80b5bc3d7a
Ibelin <extremewaysback@hotmail.com>
2015-11-06 21:04:41 +0800

    txyz2

--- origin/settings.py
+++ origin/settings.py
@@ -99,7 +99,7 @@ DATABASES={'default':{'ENGINE':'django.db.backends.postgresql_psycopg2',
                       }
           }
 '''
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+DATABASES = {'default': dj_database_url.config(default='DATABASE_URL')}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
