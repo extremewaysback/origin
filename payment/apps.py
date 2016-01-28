91ac6fd033166f4c70756b67146e790506349d74
Ibelin <extremewaysback@hotmail.com>
2016-01-28 10:22:38 +0800

    csv

---
+++ payment/apps.py
@@ -0,0 +1,11 @@
+#payment/apps.py
+
+from django.apps import AppConfig
+
+class PaymentConfig(AppConfig):
+    name='payment'
+    verbose_name='Payment'
+    
+    def ready(self):
+        #import signal handlers
+        import payment.signals
